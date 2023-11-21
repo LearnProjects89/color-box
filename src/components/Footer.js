fde19fb12a9855471067fe52b574f1d112658048
XenaSit <sitnikovaxena@gmail.com>
2023-11-21 14:14:45 -0500

    components and assets are here

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <>
+      <p>This is My Footer By Me</p>
+    </>
+  )
+}
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <>
+      <p>This Is My Header</p>
+    </>
+  )
+}
+export default Header
